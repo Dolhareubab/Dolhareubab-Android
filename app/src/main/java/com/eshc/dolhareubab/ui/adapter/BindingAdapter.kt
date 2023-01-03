cf4c88be94e074c74c937c229d0d615f54e451bc
Jung SeokJoon <eshc.joon@gmail.com>
2023-01-03 12:53:12 +0900

    [FEATURE] BindingAdapter 수정 - ImageView.load Uri parameter 추가

--- app/src/main/java/com/eshc/dolhareubab/ui/adapter/BindingAdapter.kt
+++ app/src/main/java/com/eshc/dolhareubab/ui/adapter/BindingAdapter.kt
@@ -1,6 +1,8 @@
 package com.eshc.dolhareubab.ui.adapter
 
+import android.net.Uri
 import android.widget.ImageView
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -8,6 +10,7 @@ import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.FitCenter
 import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.eshc.dolhareubab.R
 import com.eshc.dolhareubab.ui.common.ext.dpToPx
 import java.io.File
 
@@ -38,5 +41,23 @@ fun ImageView.load(
         .diskCacheStrategy(DiskCacheStrategy.ALL)
         .transform(if(corner > 0) CenterCrop() else FitCenter(),
             if(corner > 0) RoundedCorners(corner.dpToPx(this@load.context)) else null)
+        .into(this)
+}
+
+@BindingAdapter(value = ["imageUrl", "corner"], requireAll = false)
+fun ImageView.load(
+    imageUrl: Uri?,
+    corner: Int = 0
+) {
+    //if (imageUrl == null) return
+
+    Glide.with(this.context)
+        .load(imageUrl ?: AppCompatResources.getDrawable(this.context, R.drawable.bg_rect_gray_light_radi_8))
+        .diskCacheStrategy(DiskCacheStrategy.ALL)
+        .transform(
+            if (corner > 0) CenterCrop() else CenterCrop(), if (corner > 0) RoundedCorners(
+                corner.dpToPx(this@load.context)
+            ) else CenterCrop()
+        )
         .into(this)
 }
